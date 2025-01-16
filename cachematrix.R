c9ead5706884dcf751669899a4505cdcf2390a6b
c3-ivanpoblette <Ivan.Poblette@c3.ai>
2025-01-15 19:02:04 -0800

    implement cache invertible matrix through lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<- NULL
+        set<- function(){
+          x <<- x
+          m <<-NULL
+        }
+        get <- function() x
+        setresult <- function(result) m<<- result
+        getresult <- function() m
+        list(set=set,get=get,
+             setresult=setresult,
+             getresult=getresult)
 }
 
 
@@ -12,4 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if ( is.null(x$getresult()) )
+        {
+          x$setresult( solve(x$get()) )
+          return( x$getresult() )
+        }
+        message("Getting cache result")
+        x$getresult() 
 }
